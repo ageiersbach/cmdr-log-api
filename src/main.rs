2814d5e8f3514f1110abb5223aeac675548da483
Anne Geiersbach <annasazi@gmail.com>
2018-07-26 11:21:57 -0400

    Read commander name and path from stdin
    Since there are multiple args, it may be
    difficult to remember the correct order
    in future. Simplifies the input so that
    we can properly identify the commander
    and ping the api for the TOKEN to use
    in any subsequent API calls.

--- src/main.rs
+++ src/main.rs
@@ -13,35 +13,49 @@ pub mod api;
 use chrono::prelude::*;
 use std::env;
 use journal::{PlayerJournal, todays_player_journal};
+use std::io::{self, BufRead, Write};
+
+fn get_cli_string() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    let input = input.trim();
+    input.to_string()
+}
+
+fn read_player_journal(path_name: String, commander_name: String, commencement: DateTime<Utc>) {
+    println!("Before I can actually post, I must get credentials and api endpoint for {}", commander_name);
+    if let Some(tpj) = todays_player_journal(&path_name) {
+        if let Ok(pj) = PlayerJournal::read(&tpj) {
+            pj.api_payload_from(&commencement).and_then(|api| Some(api.post()));
+            //println!("What did we catch today? {:?}", pj);
+        }
+    }
+}
 
 fn main() {
+    let mut commencement = Utc::now();
     if let Some(path_name) = env::args().nth(1) {
         println!("here be the Path you gave {:?}", &path_name);
-        if let Some(tpj) = todays_player_journal(&path_name) {
-            //let time_before_read = Utc::now();
-            let time_before_read = "2018-05-10T02:13:09Z".parse::<DateTime<Utc>>().expect("could not parse date");
-
-            if let Ok(pj) = PlayerJournal::read(&tpj) {
-                pj.api_payload_from(&time_before_read).and_then(|api| Some(api.post()));
-                //println!("What did we catch today? {:?}", pj);
-            }
-        }
+        print!("commander_name: ");
+        io::stdout().flush().unwrap();
+        let commander_name = get_cli_string();
+        println!("and then i can access it here: {:?}", commander_name);
+        read_player_journal(path_name, commander_name, commencement);
     } else {
         println!("Please enter a valid path for player journals");
-    }
+        print!("path: ");
+        io::stdout().flush().unwrap();
 
-    // loop -> read file -> parse event_type, s
-    //events::hello();
-    // let player_jounral = journal::get_newest_logfile_at_path();
-    // let mut timestamp: DateTime = initialize_somehow(); // ????
-    // loop ->
-    //   player_journal.next_unread_line() ->
-    //     let event = parse_json_event(line);
-    //     match event.type ->
-    //       ShutDown => break,
-    //       API => send_http(host, event.to_http()),
-    //       _ => println!('...')
-    //     timestamp = event.timestamp;
+        let path_name = get_cli_string();
+
+        print!("commander_name: ");
+        io::stdout().flush().unwrap();
+        let commander_name = get_cli_string();
+
+        println!("Preparing to proceed with commander {}, for path: {}", commander_name, path_name);
+        read_player_journal(path_name, commander_name, commencement);
+    }
 }
 
 
